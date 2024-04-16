87aa65b0e69da80f0a324d6da88167b3038f25b0
Trickshot1722 <erickluoga1722@yahoo.com>
2024-04-17 00:51:58 +0300

    commit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+// import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import "bootstrap/dist/css/bootstrap.css";
+
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.js";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
--- frontend/src/store/configureStore.js
+++ frontend/src/store/configureStore.js
@@ -1,2 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
+import reducer from "./reducer";
 
+// eslint-disable-next-line import/no-anonymous-default-export
+export default function () {
+  return configureStore({ reducer });
+}
