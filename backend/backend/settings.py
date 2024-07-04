78852349b2d6d5daba415a09f3f67fcb00fac90b
Trickshot1722 <erickluoga1722@yahoo.com>
2024-07-04 15:06:07 +0300

    separate branch

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 from datetime import timedelta
 from decouple import config
@@ -124,6 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "frontend/build/static"),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
